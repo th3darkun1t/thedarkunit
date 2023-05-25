c943a35df4719c2334f13b38c5be82e8cd24f1ef
The Dark Unit <121042152+th3darkun1t@users.noreply.github.com>
2023-05-25 19:32:01 +0530

    Add files via upload

---
+++ src/containers/Feed/components/cards/ServiceCard.tsx
@@ -0,0 +1,13 @@
+import { CONFIG } from "site.config"
+import React from "react"
+import { AiFillCodeSandboxCircle } from "react-icons/ai"
+
+const ServiceCard: React.FC = () => {
+  if (!CONFIG.projects) return null
+  return (
+    <>
+    </>
+  )
+}
+
+export default ServiceCard
